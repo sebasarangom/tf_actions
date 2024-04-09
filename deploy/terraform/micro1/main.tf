a9ffa539dbeb92c8102e4ca54ca98edd04b1c69f
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-08 20:23:36 -0500

    Commit test files

--- deploy/terraform/micro1/main.tf
+++ deploy/terraform/micro1/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "us-east-1"
+  version = "5.44.0"
+  region  = "us-east-1"
 }
 
-resource "aws_ecrpublic_repository" "repoTest" {
+resource "aws_ecrpublic_repository" "repoTest1" {
   repository_name = "repo-test-micro1"
 }
