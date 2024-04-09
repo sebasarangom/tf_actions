5981328d5d3b64873e702c7736e6f6c4caf7ab71
Sebastian Arango <js.arangom1@uniandes.edu.co>
2024-04-08 21:41:55 -0500

    Commit test files

--- deploy/terraform/micro2/main.tf
+++ deploy/terraform/micro2/main.tf
@@ -1,5 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "tfstate-proyecto-final"
+    region = "us-east-1"
+    key    = "terraform.tfstate"
+  }
+}
+
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_ecrpublic_repository" "repoTest2" {
